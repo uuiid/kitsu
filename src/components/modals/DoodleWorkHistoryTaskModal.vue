a1e8778ac8ce461e7b6e0ada54b677d0b342c405
muzilll <634181733@qq.com>
2025-02-17 17:12:18 +0800

    添加自动灯光历史任务搜索及按名称排序

--- src/components/modals/DoodleWorkHistoryTaskModal.vue
+++ src/components/modals/DoodleWorkHistoryTaskModal.vue
@@ -1,11 +1,35 @@
 <script setup>
 import TableList from '@/components/lists/TableList.vue'
 import { doodleWorkStore } from '@/store/modules/doodlework.js'
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
+import { SearchIcon } from 'lucide-vue-next'
 
 const doodleWork = doodleWorkStore()
 doodleWork.actions.listWorkTasks()
+const inputValue = ref('')
+const inputValueModle = ref('')
 const selectedNum = ref(0)
+
+const filterTasks = computed(() => {
+  if (inputValue.value) {
+    const temp = new Map()
+    const temp_list = []
+    doodleWork.currentDoodleWorkState.historyWorkList.forEach((value, key) => {
+      if (new RegExp(`.*?${inputValue.value}.*$`, 'gmi').test(value.name)) {
+        temp_list.push(value)
+      }
+    })
+    temp_list.sort((a, b) => {
+      return a.name.localeCompare(b.name)
+    })
+    temp_list.forEach(item => {
+      temp.set(item.id, item)
+    })
+    return temp
+  }
+  return doodleWork.currentDoodleWorkState.historyWorkList
+})
+
 const onAddData = files => {
   doodleWork.currentDoodleWorkState.addFilesData(files)
 }
@@ -53,13 +77,30 @@ const onSelected = task => {
         <h1 class="title">
           {{ $t('doodle_work.add_doodle_work') }}
         </h1>
+        <div class="has-right">
+          <div class="search-field-main">
+            <span class="search-icon">
+              <search-icon :size="20" />
+            </span>
+            <input
+              ref="search-field"
+              class="input"
+              :placeholder="$t('scan_project.name')"
+              v-model.trim="inputValueModle"
+              @keydown.enter="inputValue = inputValueModle"
+              @input="
+                inputValueModle ? undefined : (inputValue = inputValueModle)
+              "
+            />
+          </div>
+        </div>
         <table-list
           class="table-list"
           :name="`执行${selectedNum > 0 ? `(${selectedNum})` : ''}`"
           :table-header-filed="
             doodleWork.currentDoodleWorkState.tableHeaderFiled
           "
-          :body-list="doodleWork.currentDoodleWorkState.historyWorkList"
+          :body-list="filterTasks"
           :is-drop="false"
           :is-show-submit="true"
           :is-selectable="true"
@@ -109,5 +150,38 @@ input[type='number']::-webkit-outer-spin-button {
 
 .input-checkbox {
   height: 30px;
+}
+
+.search-field-main {
+  margin-bottom: 5px;
+  //display: flex;
+  padding-top: 0;
+  position: relative;
+
+  .input {
+    font-size: 0.8em;
+    border-radius: 10px;
+    padding-left: 40px;
+  }
+
+  .search-icon {
+    position: absolute;
+    color: $grey;
+    z-index: 4;
+    top: 6px;
+    left: 10px;
+  }
+}
+
+.has-right {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  margin-bottom: 5px;
+}
+
+.input {
+  min-width: 180px;
 }
 </style>
