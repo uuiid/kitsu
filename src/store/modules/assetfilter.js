8bef41f72b8894c66c4d6b6c272e8fa47f0b4cc5
muzilll <634181733@qq.com>
2025-02-18 17:44:18 +0800

    添加资产按任务过滤

--- src/store/modules/assetfilter.js
+++ src/store/modules/assetfilter.js
@@ -1,6 +1,8 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import i18n from '@/lib/i18n.js'
+import people from '@/store/modules/people.js'
+import tasks from '@/store/modules/tasks.js'
 
 function initState() {
   return {
@@ -8,6 +10,7 @@ function initState() {
     filteredAssets: [],
     oldDisplayedAssetsByType: null,
     treeFilterData: [],
+    treeFilterMap: new Map(),
     filters: []
   }
 }
@@ -29,6 +32,11 @@ export const assetFilterStore = defineStore('assetFilterStore', () => {
     values: [0],
     parent: 'data'
   })
+  state.value.assetFilters.set('assignees', {
+    id: 'assignees',
+    values: [],
+    parent: 'task'
+  })
   const getters = {}
   const actions = {
     filteringAsset: (asset, temp, keys) => {
@@ -37,85 +45,113 @@ export const assetFilterStore = defineStore('assetFilterStore', () => {
         for (let i = 0; i < state.value.assetFilters.size; i++) {
           const item = state.value.assetFilters.get(keys[i])
           const key = keys[i]
-          const ch = {
-            id: '',
-            label: '',
-            num: 1,
-            parent: key,
-            value: ''
-          }
-          if (item.parent) {
-            ch.id = `${key}:${asset[item.parent][key]}`
-            ch.label = asset[item.parent][key]
-            ch.value = asset[item.parent][key]
-            if (
-              asset[item.parent][key] === undefined ||
-              asset[item.parent][key] === ''
-            ) {
-              ch.id = `${key}:undefined`
-              ch.label = '其他'
-              ch.value = undefined
+          if (item.parent !== 'task') {
+            const ch = {
+              id: '',
+              label: '',
+              num: 1,
+              parent: key,
+              value: ''
             }
-          } else {
-            ch.id = `${key}:${asset[key]}`
-            ch.label = asset[key]
-            ch.value = asset[key]
-            if (asset[key] === undefined || asset[key] === '') {
-              ch.id = `${key}:undefined`
-              ch.label = '其他'
-              ch.value = undefined
-            }
-          }
-          let filter_value = false
-          if (i === 0) {
-            filter_value = true
-          } else {
-            for (let j = 0; j < i; j++) {
-              const last_item = state.value.assetFilters.get(keys[j])
-              const last_key = keys[j]
-              let asset_value = asset[last_key]
-              if (last_item.parent) {
-                asset_value = asset[last_item.parent][last_key]
-                if (asset[key] === '') asset_value = undefined
-              }
-              if (last_item.values.includes(asset_value)) {
-                filter_value = true
-              } else {
-                filter_value = false
-                break
-              }
-            }
-          }
-          if (i === state.value.assetFilters.size - 1 && filter_value) {
-            value = item.values.includes(ch.value)
-          }
-          if (filter_value) {
-            if (temp.has(item.id)) {
-              temp.get(item.id).num += 1
-              const children = temp
-                .get(item.id)
-                .children.filter(i => i.id === ch.id)
-              if (children.length === 0) {
-                temp.get(item.id).children.push(ch)
-                temp.get(item.id).children.sort((a, b) => {
-                  return String(a.label).localeCompare(String(b.label))
-                })
-              } else {
-                children[0].num += 1
+            if (item.parent) {
+              ch.id = `${key}:${asset[item.parent][key]}`
+              ch.label = asset[item.parent][key]
+              ch.value = asset[item.parent][key]
+              if (
+                asset[item.parent][key] === undefined ||
+                asset[item.parent][key] === ''
+              ) {
+                ch.id = `${key}:undefined`
+                ch.label = '其他'
+                ch.value = undefined
               }
             } else {
-              temp.set(item.id, {
-                id: item.id,
-                label: i18n.global.t('doodle_asset_tree.fields.' + item.id),
-                num: 1,
-                value: item.id,
-                children: [ch]
-              })
+              ch.id = `${key}:${asset[key]}`
+              ch.label = asset[key]
+              ch.value = asset[key]
+              if (asset[key] === undefined || asset[key] === '') {
+                ch.id = `${key}:undefined`
+                ch.label = '其他'
+                ch.value = undefined
+              }
+            }
+            let filter_value = false
+            if (i === 0) {
+              filter_value = true
+            } else {
+              for (let j = 0; j < i; j++) {
+                const last_item = state.value.assetFilters.get(keys[j])
+                const last_key = keys[j]
+                let asset_value = asset[last_key]
+                if (last_item.parent) {
+                  asset_value = asset[last_item.parent][last_key]
+                  if (asset[key] === '') asset_value = undefined
+                }
+                if (last_item.values.includes(asset_value)) {
+                  filter_value = true
+                } else {
+                  filter_value = false
+                  break
+                }
+              }
+            }
+            if (i === state.value.assetFilters.size - 1 && filter_value) {
+              value = item.values.includes(ch.value)
+            }
+            if (filter_value) {
+              actions.addTreeFilterItem(temp, ch, item)
+            }
+          } else {
+            for (const task_id of asset.tasks) {
+              const task = tasks.state.taskMap.get(task_id)
+              if (item.id === 'assignees') {
+                for (const assignee of task.assignees) {
+                  const ch = {
+                    id: '',
+                    label: '',
+                    num: 1,
+                    parent: key,
+                    value: ''
+                  }
+                  ch.id = `${key}:${assignee}`
+                  ch.label = people.getters.personMap().get(assignee).first_name
+                  ch.value = assignee
+                  ch.parent = key
+                  actions.addTreeFilterItem(temp, ch, item)
+                }
+              }
             }
           }
         }
       }
       return value
+    },
+    addTreeFilterItem: (temp, ch, item) => {
+      if (temp.has(item.id)) {
+        temp.get(item.id).num += 1
+        let children = null
+        temp.get(item.id).children.forEach(child => {
+          if (child.label === ch.label) {
+            children = child
+          }
+        })
+        if (children === null) {
+          temp.get(item.id).children.push(ch)
+          temp.get(item.id).children.sort((a, b) => {
+            return String(a.label).localeCompare(String(b.label))
+          })
+        } else {
+          children.num += 1
+        }
+      } else {
+        temp.set(item.id, {
+          id: item.id,
+          label: i18n.global.t('doodle_asset_tree.fields.' + item.id),
+          num: 1,
+          value: item.id,
+          children: [ch]
+        })
+      }
     },
     filteringAssets: assets => {
       const temp = new Map()
