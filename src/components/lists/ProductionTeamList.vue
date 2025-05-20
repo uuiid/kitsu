41b577e65a28cc35431607ee09c63c5a16951f18
muzilll <634181733@qq.com>
2025-05-20 18:24:20 +0800

    添加成员列表按部门过滤

--- src/components/lists/ProductionTeamList.vue
+++ src/components/lists/ProductionTeamList.vue
@@ -1,4 +1,37 @@
 <template>
+  <custom-table-header-menu
+    ref="headerMenu"
+    @blur="departmentsMenu = false"
+    v-if="departmentsMenu"
+    :style="`top:${departmentsMenuPosition.Y}px;left:${departmentsMenuPosition.X}px;`"
+  >
+    <template #list>
+      <div
+        class="departments-list"
+        :key="department"
+        v-for="department in departmentsList"
+      >
+        <div class="departments-list">
+          <input
+            type="checkbox"
+            @mousedown.prevent
+            :checked="selectedDepartments.includes(department)"
+            @change="onChange(department)"
+          />
+          <span
+            style="
+              width: 100%;
+              cursor: pointer;
+              user-select: none;
+              color: var(--text);
+            "
+            @click="onChange(department)"
+            >{{ departmentMap.get(department).name }}</span
+          >
+        </div>
+      </div>
+    </template>
+  </custom-table-header-menu>
   <div class="data-list">
     <div class="datatable-wrapper">
       <table class="datatable">
@@ -17,13 +50,27 @@
               {{ $t('people.list.role') }}
             </th>
             <th scope="col">
-              {{ $t('people.list.departments') }}
+              <div style="position: relative" ref="departmentsRef">
+                <span>
+                  {{ $t('people.list.departments') }}
+                </span>
+                <span
+                  class="metadata-menu-button header-icon"
+                  @click="showHeaderMenu"
+                >
+                  <chevron-down-icon :size="12" />
+                </span>
+              </div>
             </th>
             <th scope="col" class="actions" v-if="isCurrentUserManager"></th>
           </tr>
         </thead>
         <tbody class="datatable-body" v-if="!isEmpty">
-          <tr class="datatable-row" :key="person.id" v-for="person in entries">
+          <tr
+            class="datatable-row"
+            :key="person.id"
+            v-for="person in filteredEntries"
+          >
             <people-name-cell
               class="name datatable-row-header"
               :person="person"
@@ -63,13 +110,28 @@ import { mapGetters, mapActions } from 'vuex'
 
 import DepartmentNamesCell from '@/components/cells/DepartmentNamesCell.vue'
 import PeopleNameCell from '@/components/cells/PeopleNameCell.vue'
+import { ChevronDownIcon } from 'lucide-vue-next'
+import CustomTableHeaderMenu from '@/components/cells/CustomTableHeaderMenu.vue'
 
 export default {
   name: 'production-team-list',
 
   components: {
+    CustomTableHeaderMenu,
+    ChevronDownIcon,
     DepartmentNamesCell,
     PeopleNameCell
+  },
+  data() {
+    return {
+      departmentsMenu: false,
+      selectedDepartments: [],
+      allDepartments: [],
+      departmentsMenuPosition: {
+        X: 0,
+        Y: 0
+      }
+    }
   },
 
   props: {
@@ -78,10 +140,31 @@ export default {
       default: () => []
     }
   },
+  mounted() {},
 
   computed: {
-    ...mapGetters(['isCurrentUserManager']),
-
+    ...mapGetters(['isCurrentUserManager', 'departmentMap']),
+    filteredEntries() {
+      if (this.selectedDepartments.length === 0) {
+        return this.entries
+      } else {
+        return this.entries.filter(entry => {
+          return this.checkIncludeList(
+            entry.departments,
+            this.selectedDepartments
+          )
+        })
+      }
+    },
+    departmentsList() {
+      const temp = []
+      this.entries.forEach(entry => {
+        entry.departments.forEach(department => {
+          if (!temp.includes(department)) temp.push(department)
+        })
+      })
+      return temp
+    },
     isEmpty() {
       return !this.entries?.length
     }
@@ -92,6 +175,34 @@ export default {
 
     removePerson(person) {
       this.removePersonFromTeam(person)
+    },
+    onChange(department) {
+      if (this.selectedDepartments.includes(department)) {
+        const index = this.selectedDepartments.indexOf(department)
+        this.selectedDepartments.splice(index, 1)
+      } else this.selectedDepartments.push(department)
+    },
+    showHeaderMenu() {
+      this.departmentsMenu = true
+      this.$nextTick(() => {
+        this.$refs.headerMenu.focus()
+        const rect =
+          this.$refs.departmentsRef.$el?.getBoundingClientRect?.() ??
+          this.$refs.departmentsRef.getBoundingClientRect?.()
+
+        if (rect) {
+          this.departmentsMenuPosition.X = rect.left + window.scrollX + 35
+          this.departmentsMenuPosition.Y = rect.top + window.scrollY + 25
+        }
+      })
+    },
+    checkIncludeList(list = [], list2 = []) {
+      if (list && list2) {
+        for (const i of list) {
+          if (list2.includes(i)) return true
+        }
+      }
+      return false
     }
   }
 }
@@ -138,5 +249,19 @@ export default {
 
 .footer-info {
   color: var(--text);
+}
+
+.departments-list {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  gap: 5px;
+  list-style: none;
+  min-width: 100px;
+  color: var(--color);
+
+  :hover {
+    background-color: var(--background-selectable);
+  }
 }
 </style>
