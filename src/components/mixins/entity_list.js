e00573d9c292bc187afa193427c55b9b69c90895
muzilll <634181733@qq.com>
2025-07-28 15:08:33 +0800

    调整columnsID排序

--- src/components/mixins/entity_list.js
+++ src/components/mixins/entity_list.js
@@ -72,7 +72,7 @@ export const entityListMixin = {
           this.validationColumnsIsInDepartmentFilter(columnId)
       )
       const name = this.taskTypeMap.get(this.displayedValidationColumns[0]).name
-      if (name === '角色') {
+      if (name === '角色' && columns.length > 1) {
         this.swap(columns, 0, 1)
       }
       return columns
