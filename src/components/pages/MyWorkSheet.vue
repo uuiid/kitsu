1dec9350deec7b211929a338245f765106817212
muzilll <634181733@qq.com>
2025-08-01 10:20:31 +0800

    调整导出工作表名称字段

--- src/components/pages/MyWorkSheet.vue
+++ src/components/pages/MyWorkSheet.vue
@@ -710,8 +710,9 @@ export default {
       const duration = Number(t.work_duration / (1000 * 1000 * 60 * 60 * 8))
       line.push(duration)
       line.push(t.work_remark)
+      console.log(t)
       if (t.work_user_remark) line.push(`${t.task_name}(${t.work_user_remark})`)
-      else line.push(t.task_name)
+      else line.push(t.entity_name || t.task_name)
       const level = t.entity_deng_ji
       line.push(level)
       return line
