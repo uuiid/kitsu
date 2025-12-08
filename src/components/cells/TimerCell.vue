83e714cd894a09b11b1eb85aa2f5b9e453265cf4
muzilll <634181733@qq.com>
2025-12-08 18:10:57 +0800

    调整镜头下载资产状态,运行时间

--- src/components/cells/TimerCell.vue
+++ src/components/cells/TimerCell.vue
@@ -70,8 +70,9 @@ const start = () => {
         })
         time.value = doodleWork.actions.formatDiffTime(all_time)
       } else {
+        console.log(props.task)
         time.value = doodleWork.actions.formatDiffTime(
-          new Date(props.task.end_time) - new Date(props.task.start_time)
+          new Date(props.task.end_time) - new Date(props.task.submit_time)
         )
       }
     }
