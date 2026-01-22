5515d652d052883fc659723b2ffc3d651f1c8362
muzilll <634181733@qq.com>
2026-01-22 15:20:56 +0800

    修复Electron环境下的Cookies设置逻辑，确保用户登出时正确清除Cookies

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -59,6 +59,7 @@ const auth = {
         console.error(err)
         callback(err)
       }
+      if (navigator.userAgent.includes('Electron')) window.api.setCookies('')
       store.commit(USER_LOGOUT)
       callback()
     })
