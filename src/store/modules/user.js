eee73612b4b2ee0bee1875cdec1f1ab402f0e66e
muzilll <634181733@qq.com>
2026-02-11 10:54:45 +0800

    调整制片编辑和删除资产权限

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -146,7 +146,8 @@ const getters = {
   isCurrentUserSupervisor: state =>
     state.user && state.user.role === 'supervisor',
   isCurrentUserSupervisorAbove: state =>
-    state.user && ['supervisor', 'manager', 'admin'].includes(state.user.role),
+    state.user &&
+    ['supervisor', 'manager', 'admin', 'producer'].includes(state.user.role),
   isCurrentUserClient: state => state.user && state.user.role === 'client',
   isCurrentUserVendor: state => state.user && state.user.role === 'outsource',
   isSaveProfileLoading: state => state.isSaveProfileLoading,
