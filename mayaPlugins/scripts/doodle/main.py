fde4c3a7c1946ce0d0fc30b55d5514853a3c4f22
muzilll <634181733@qq.com>
2026-03-23 16:18:36 +0800

    修复 get_maya_main_window 方法中的类型转换错误，将 long 类型改为 int 类型

--- mayaPlugins/scripts/doodle/main.py
+++ mayaPlugins/scripts/doodle/main.py
@@ -17,7 +17,7 @@ class main(object):
   @staticmethod
   def get_maya_main_window():
     main_window_ptr = omui.MQtUtil.mainWindow()
-    return wrapInstance(long(main_window_ptr), QWidget)
+    return wrapInstance(int(main_window_ptr), QWidget)
 
   @staticmethod
   def add_doodle_tool_widget(*args):
